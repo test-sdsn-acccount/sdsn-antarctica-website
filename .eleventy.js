f157cd6eca52efd9bc8e6686f0d503b2672a2624
Sophia Huynh <Sph.huynh@gmail.com>
2020-07-21 19:18:17 +1000

    Banner block is ready

--- .eleventy.js
+++ .eleventy.js
@@ -2,8 +2,11 @@ module.exports = function(config) {
 
     // Layout aliases
     config.addLayoutAlias('base', 'layouts/base.njk');
-    config.addLayoutAlias('page-home', 'layouts/page-home.njk');
+    config.addLayoutAlias('page', 'layouts/page.njk');
+    config.addLayoutAlias('homepage', 'layouts/homepage.njk');
+    config.addLayoutAlias('home', 'layouts/home.njk');
     config.addLayoutAlias('banner', 'layouts/banner.njk');
+    config.addLayoutAlias('section-banner', 'layouts/section-banner.njk');
     config.addLayoutAlias('contact-us', 'layouts/contact-us.njk');
 
     // Passthrough copy
@@ -12,6 +15,12 @@ module.exports = function(config) {
     config.addPassthroughCopy('src/assets/images');
     config.addPassthroughCopy('src/assets/uploads');
     config.addPassthroughCopy('src/content');
+
+    // Custom collections
+    config.addCollection('homepageSections', collection => {
+        return [...collection.getFilteredByGlob('./src/content/homepage/*.md')]
+        .reverse();
+      });
 
     return {
 		pathPrefix: "/",
